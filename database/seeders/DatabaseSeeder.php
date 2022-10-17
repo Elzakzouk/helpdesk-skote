75bba6d3b0456765c7e1989060ccbca745d79915
Abdulrahamn <abdulrahman.1996z@hotmail.com>
2022-10-17 16:12:03 +0300

    + Adding seeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RegionSeeder::class,
+            CitySeeder::class,
+            // RoleSeeder::class,
+            // UserSeeder::class,
+            // TeamSeeder::class,
+            // TicketType::class,
+            // TicketStatus::class,
+            // TicketActivity::class,
+            // TicketPriority::class
+        ]);
     }
 }
